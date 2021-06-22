7daef23dc7417c8c6cfc2e89a3f9627ea20f3790
nguoitacu@gmail.com <nguoitacu@gmail.com>
2021-06-22 21:19:31 +0700

    add antd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import { Layout,PageHeader} from "antd";
+const { Content } = Layout;
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="ant-content">
+          <Content style={{ margin: '80px 30px 0 230px' }}>
+              <PageHeader  title="Products" /> 
+                    <div className="block-area">                        
+                    {/* <TableProducts></TableProducts>  */}a
+              </div>            
+          </Content>           
+      </div>
     </div>
   );
 }
